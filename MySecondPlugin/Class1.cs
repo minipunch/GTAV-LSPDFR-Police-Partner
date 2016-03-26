32bcaccba584037747561a3980beb685b55c5f5e
Joshua G. Yolles <josh@whaicehockey.com>
2016-03-25 19:40:43 -0700

    Fixed enter vehicle warping
    Changed some of the native calls to now use RPH's built in native task
    methods.
    
    TODO:
    - Add a "stand down" feature.
    - Add a limit to one partner at a time.

--- MySecondPlugin/Class1.cs
+++ MySecondPlugin/Class1.cs
@@ -14,70 +14,76 @@ namespace bodyguardPlugin
 
     internal static class EntryPoint
     {
+        // our bodyguard group
+        public static Group bodyguardGroup = null;
+        // our bodyguard
+        public static Ped bodyguard = null;
+        // the player
+        public static Ped playerPed = null;
+
         /// <summary>
         /// Defines the entry point of the plugin.
         /// </summary>
         private static void Main()
         {
-            // our bodyguard group
-            Group bodyguardGroup = null;
-            // our bodyguard
-            Ped bodyguard = null;
-
             while (true)
             {
-				// Key to spawn bodyguard (F9)
+                // Yield to other plugins
+                GameFiber.Yield();
+                // Key to spawn bodyguard (F9)
                 if (Game.IsKeyDown(Keys.F9))
                 {
-					// Create bodyguard group
-                    bodyguardGroup = Game.LocalPlayer.Character.Group;
+                    // assign playerPed character to user's character
+                    playerPed = Game.LocalPlayer.Character;
+                    // Create bodyguard group
+                    bodyguardGroup = playerPed.Group;
 					// set spawn locatation for bodyguard
-                    Vector3 spawnLocation = Game.LocalPlayer.Character.GetOffsetPosition(Vector3.RelativeFront * 10f);
+                    Vector3 spawnLocation = playerPed.GetOffsetPosition(Vector3.RelativeFront * 10f);
                     // spawn new bodyguard
 					bodyguard = new Ped("S_M_Y_Sheriff_01", spawnLocation, 0f);
                     bodyguard.BlockPermanentEvents = true;
-					// give him a weapon
+                    // give him a pistol
                     NativeFunction.Natives.GiveWeaponToPed(bodyguard,0x1B06D571,450, true, true);
 					// make him a cop
                     NativeFunction.Natives.SetPedAsCop(bodyguard, true);
 					// display notification
-                    Game.DisplayNotification("Bodyguard spawned!");
+                    Game.DisplayNotification("Your partner is ready for patrol!");
                 }
 
 				// if bodyguard exists, but for some reason is not in the player's group
                 if (bodyguard != null && (bodyguardGroup.Count - 1) < 1)
                 {
-					// make sure he is always a member of the player group
+                    // make sure he is always a member of the player group
                     bodyguardGroup.AddMember(bodyguard);
                 }
 
                 // If the bodyguard has been created, and your player is in a police car, but the bodyguard is not
-                if (bodyguard != null && Game.LocalPlayer.Character.IsInAnyPoliceVehicle && !(bodyguard.IsInAnyPoliceVehicle))
+                if (bodyguard != null && playerPed.IsInAnyPoliceVehicle && !(bodyguard.IsInAnyPoliceVehicle))
                 {
 
                     // set the proper car target for entry
-                    Vehicle leaderVehicle = Game.LocalPlayer.Character.CurrentVehicle;
+                    Vehicle leaderVehicle = playerPed.CurrentVehicle;
 
+                    // OLD VERSION : NativeFunction.Natives.TaskGoToEntity(bodyguard, leaderVehicle, 5000, 2.0f, 200, 1073741824, 0);
                     // move the bodyguard to the targetted car
-                    NativeFunction.Natives.TaskGoToEntity(bodyguard, leaderVehicle, 5000, 2.0f, 200, 1073741824, 0);
+                    bodyguard.Tasks.GoToOffsetFromEntity(leaderVehicle, 0, 5.0f, 3.0f, 2.0f).WaitForCompletion();
 
-                    // give some time for bodyguard to get to car before handling next condition
-                    GameFiber.Wait(5000);
+                    // OLD VERSION: NativeFunction.Natives.TaskOpenVehicleDoor(bodyguard, leaderVehicle, 0, 1, 2.0f);
+                    // enter the vehicle
+                    bodyguard.Tasks.EnterVehicle(leaderVehicle, 0).WaitForCompletion();
 
-                    NativeFunction.Natives.TaskOpenVehicleDoor(bodyguard, leaderVehicle, 0, 1, 2.0f);
-                    NativeFunction.Natives.TaskEnterVehicle(bodyguard, leaderVehicle, 1, 0, 2.0f, 1, 0);
                 }
 
                 // If the bodyguard is in a vehicle, while you are not
-                if(bodyguard != null && bodyguard.IsInAnyPoliceVehicle && Game.LocalPlayer.Character.DistanceTo2D(bodyguard.GetOffsetPosition(Vector3.RelativeFront)) > 5f)
+                if(bodyguard != null && bodyguard.IsInAnyPoliceVehicle && playerPed.DistanceTo2D(bodyguard.GetOffsetPosition(Vector3.RelativeFront)) > 5f)
                 {
-					// make the bodyguard get out and follow you
-                    NativeFunction.Natives.TaskLeaveVehicle(bodyguard, bodyguard.CurrentVehicle, 1);
-                    NativeFunction.Natives.TaskGoToEntity(bodyguard, Game.LocalPlayer.Character, 5000, 2.0f, 200, 1073741824, 0);
+                    // OLD VERSION: NativeFunction.Natives.TaskLeaveVehicle(bodyguard, bodyguard.CurrentVehicle, 1);
+                    // OLD VERSION: NativeFunction.Natives.TaskGoToEntity(bodyguard, playerPed, 5000, 2.0f, 200, 1073741824, 0);
+                    // make the bodyguard get out and follow you
+                    bodyguard.Tasks.LeaveVehicle(LeaveVehicleFlags.None).WaitForCompletion();
+                    bodyguard.Tasks.GoToOffsetFromEntity(playerPed, 0, 5.0f, 3.0f, 2.0f).WaitForCompletion();
                 }
 
-				// Yield to other plugins
-                GameFiber.Yield();
             }
 
         }
